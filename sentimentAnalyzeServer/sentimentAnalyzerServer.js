ca5a79d3e575679cf4611e3afb99867e065ee816
nitrek <koolestnitesh@gmail.com>
2021-09-20 00:52:12 +0530

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -15,13 +15,10 @@ variables that you set up in the .env file*/
 const dotenv = require('dotenv');
 dotenv.config();
 
-const api_key = process.env.API_KEY;
-const api_url = process.env.API_URL;
-
 function getNLUInstance() {
-    /*Type the code to create the NLU instance and return it.
-    You can refer to the image in the instructions document
-    to do the same.*/
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
     const { IamAuthenticator } = require('ibm-watson/auth');
 
